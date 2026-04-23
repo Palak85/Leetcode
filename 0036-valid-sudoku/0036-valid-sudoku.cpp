fc98bde05bf6f1961352781ba2cb6a1dddde9245
Palak Kumari <149322374+Palak85@users.noreply.github.com>
2026-04-23 22:25:51 +0530

    Time: 4 ms (41.62%), Space: 23.3 MB (48.8%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<int>> rows(9, vector<int>(9, 0));
+        vector<vector<int>> cols(9, vector<int>(9, 0));
+        vector<vector<int>> boxes(9, vector<int>(9, 0));
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+
+                int num = board[i][j] - '1'; // map '1'-'9' → 0-8
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                    return false;
+                }
+
+                rows[i][num] = cols[j][num] = boxes[boxIndex][num] = 1;
+            }
+        }
+
+        return true;
+    }
+};
