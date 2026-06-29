4fc3b5f69a5c05676da77d1030957855477d68f2
Palak Kumari <149322374+Palak85@users.noreply.github.com>
2026-06-29 19:13:24 +0530

    Time: 1 ms (15.75%), Space: 22.8 MB (19.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int k = 1;
         for(int i=1; i<n; i++){
-            if(nums[i] !=  nums[i-1]){
+            if(nums[i] != nums[k - 1]){
                 nums[k] = nums[i];
                 k++;
             }
